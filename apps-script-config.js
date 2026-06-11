f4005ed2911c6314a2dc7be1a183e1ad3f170ccb
Amarneni Amogh Chowdary <amoghchowddaryamaraneni@gmail.com>
2026-06-11 12:41:18 +0530

    Connect Apps Script backend

--- apps-script-config.js
+++ apps-script-config.js
@@ -1,4 +1,4 @@
 // CA Academy Website - runtime backend configuration
 // After deploying Google Apps Script, paste the Web App URL below.
 // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
-window.CA_ACADEMY_APPS_SCRIPT_URL = "https://script.google.com/macros/s/PASTE_DEPLOYMENT_ID_HERE/exec";
+window.CA_ACADEMY_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxRlRktDUdHzd1voFoQRvtJZLsS8X-tRc9anHGQrmGv2tjRCr78KGoKGp-3z8pJVfb7Uw/exec";
