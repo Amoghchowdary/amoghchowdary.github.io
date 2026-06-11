5d1c7f30a45357c8df7125730e9d67b7ce3dbb7a
Amarneni Amogh Chowdary <amoghchowddaryamaraneni@gmail.com>
2026-06-11 12:11:38 +0530

    Initial CA Academy website hosting

---
+++ apps-script-config.js
@@ -0,0 +1,4 @@
+// CA Academy Website - runtime backend configuration
+// After deploying Google Apps Script, paste the Web App URL below.
+// Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+window.CA_ACADEMY_APPS_SCRIPT_URL = "https://script.google.com/macros/s/PASTE_DEPLOYMENT_ID_HERE/exec";
