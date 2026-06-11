e88c71677bdea2ffc4bd494a0d09373e12593ab0
Amarneni Amogh Chowdary <amoghchowddaryamaraneni@gmail.com>
2026-06-11 13:23:40 +0530

    Update Apps Script deployment URL

--- apps-script-config.js
+++ apps-script-config.js
@@ -1,4 +1,4 @@
 // CA Academy Website - runtime backend configuration
 // After deploying Google Apps Script, paste the Web App URL below.
 // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
-window.CA_ACADEMY_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxRlRktDUdHzd1voFoQRvtJZLsS8X-tRc9anHGQrmGv2tjRCr78KGoKGp-3z8pJVfb7Uw/exec";
+window.CA_ACADEMY_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwmRgL_T9Qr6_OCgBgR3eskavEG-JO5IPgqttlj4i96dpn80UmmauAQEryHeIzX50Ytug/exec";
